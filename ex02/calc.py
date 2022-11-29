ca0fbdfbc6f33ab7d192464ffedb705f598cc085
tsujimura <c0a21085f4@edu.teu.ac.jp>
2022-11-29 16:29:07 +0900

    追加：πの追加

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,7 +4,7 @@ import math
 
 root = tk.Tk()
 root.title("電卓")
-root.geometry("265x475")
+root.geometry("380x475")
 
 def button_click(event):
     btn = event.widget
@@ -25,6 +25,8 @@ def button_click(event):
         entry.delete(len(entry.get())-1,tk.END)
     elif txt == "AC": #オールクリア
         entry.delete(0, tk.END)
+    elif txt == "π":
+        entry.insert(tk.END, math.pi) 
     else:
         entry.insert(tk.END, txt)
     #tkm.showinfo(txt, f"{txt}ボタンが押されました")
@@ -38,13 +40,14 @@ num_list.append("=")
 num_list.append("C")
 num_list.append(".")
 num_list.append("AC")
+num_list.append("π")
 
 
 #print(num_list)
 
 for num in num_list:
     if type(num) is int:
-        button = tk.Button(root, text = str(num), font = ("",20), width = 4, height = 2, bg="#f5f5f5")
+        button = tk.Button(root, text = str(num), font = ("",20), width = 6, height = 2, bg="#f5f5f5")
         button.bind("<1>", button_click)
         if num % 3 == 0 and num != 0:
             button.grid(row = 6 - math.ceil(num/3), column = 0)
@@ -55,7 +58,7 @@ for num in num_list:
         elif num == 0:
             button.grid(row = 6 - math.ceil(num/3), column = 1)
     elif type(num) is str:
-        button = tk.Button(root, text = num, font = ("",20), width = 4, height = 2, bg="#c0c0c0")
+        button = tk.Button(root, text = num, font = ("",20), width = 6, height = 2, bg="#c0c0c0")
         button.bind("<1>", button_click)
         if num == "+":
             button.grid(row = 5, column = 3)
@@ -68,13 +71,15 @@ for num in num_list:
         elif num == "=":
             button.grid(row = 6, column = 3)
         elif num == "C":
-            button.grid(row = 1, column = 2)
-        elif num == "AC":
             button.grid(row = 1, column = 3)
+        elif num == "AC":
+            button.grid(row = 1, column = 2)
         elif num == ".":
             button.grid(row = 6, column = 2)
+        elif num == "π":
+            button.grid(row = 2, column = 0)
         
-entry = tk.Entry(justify="right", width=16, font=("", 18))
-entry.grid(row = 0, columnspan=5)
+entry = tk.Entry(justify="right", width=30, font=("", 18))
+entry.grid(row = 0, columnspan=4)
 
 root.mainloop()
