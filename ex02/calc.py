cadac12dc1424e0e8894e193a6d4efa5b11518af
tsujimura <c0a21085f4@edu.teu.ac.jp>
2022-11-29 14:39:16 +0900

    問題5,修正済

--- ex02/calc.py
+++ ex02/calc.py
@@ -12,26 +12,25 @@ def button_click(event):
     tkm.showinfo(txt, f"{txt}ボタンが押されました")
 
 num_list = [n for n in range(10)]
+num_list.append("+")
+num_list.append("=")
 #print(num_list)
 
 for num in num_list:
     button = tk.Button(root, text = str(num), font = ("",30), width = 4, height = 2)
     button.bind("<1>", button_click)
-    if num % 3 == 0:
-        button.grid(row = 4 - math.ceil(num/3), column= 0)
-    elif num % 3 == 1:
-        button.grid(row = 4 - math.ceil(num/3), column= 2)
-    elif num % 3 == 2:
-        button.grid(row = 4 - math.ceil(num/3), column= 1)   
-
-button = tk.Button(root, text = "+", font = ("",30), width = 4, height = 2)
-button.bind("<1>", button_click)
-button.grid(row = 4 , column= 1)
-
-button = tk.Button(root, text = "=", font = ("",30), width = 4, height = 2)
-button.bind("<1>", button_click)
-button.grid(row = 4 , column= 2)
-
+    if type(num) is int:
+        if num % 3 == 0:
+            button.grid(row = 4 - math.ceil(num/3), column = 0)
+        elif num % 3 == 1:
+            button.grid(row = 4 - math.ceil(num/3), column = 2)
+        elif num % 3 == 2:
+            button.grid(row = 4 - math.ceil(num/3), column = 1)
+    elif type(num) is str:
+        if num == "+":
+            button.grid(row = 4, column = 1)
+        elif num == "=":
+            button.grid(row = 4, column = 2)
         
 entry = tk.Entry(justify="right", width=10, font=("", 40))
 entry.grid(row = 0, columnspan=3)
