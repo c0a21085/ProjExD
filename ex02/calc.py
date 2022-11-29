31f84507544d362d7d7d55a382c5d88e4096e678
tsujimura <c0a21085f4@edu.teu.ac.jp>
2022-11-29 14:25:26 +0900

    問題4

--- ex02/calc.py
+++ ex02/calc.py
@@ -18,10 +18,13 @@ for num in num_list:
     button = tk.Button(root, text = str(num), font = ("",30), width = 4, height = 2)
     button.bind("<1>", button_click)
     if num % 3 == 0:
-        button.grid(row = 3 - math.ceil(num/3), column= 0)
+        button.grid(row = 4 - math.ceil(num/3), column= 0)
     elif num % 3 == 1:
-        button.grid(row = 3 - math.ceil(num/3), column= 2)
+        button.grid(row = 4 - math.ceil(num/3), column= 2)
     elif num % 3 == 2:
-        button.grid(row = 3 - math.ceil(num/3), column= 1)   
+        button.grid(row = 4 - math.ceil(num/3), column= 1)   
         
+entry = tk.Entry(justify="right", width=10, font=("", 40))
+entry.grid(row = 0, columnspan=3)
+
 root.mainloop()
