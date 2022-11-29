9dfbfdad364904fbc271acbf2998ca82935dc8ff
tsujimura <c0a21085f4@edu.teu.ac.jp>
2022-11-29 16:13:26 +0900

    追加：クリア,オールクリア,エラーの対応

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,18 +4,27 @@ import math
 
 root = tk.Tk()
 root.title("電卓")
-root.geometry("400x500")
+root.geometry("265x475")
 
 def button_click(event):
     btn = event.widget
     txt = btn["text"]
-    if txt == "=":
-        total = eval(entry.get())
+    if txt == "=": #合計
+        try:
+            total = eval(entry.get())
+        except(SyntaxError or ZeroDivisionError):
+            entry.delete(0, tk.END)
+            entry.insert(tk.END, "Error")
+        else:
+            entry.delete(0, tk.END)
+            entry.insert(tk.END, total)
+    elif txt == "C": #一文字クリア
+        entry.delete(len(entry.get())-1,tk.END)
+    elif txt == "AC": #オールクリア
         entry.delete(0, tk.END)
-        entry.insert(tk.END, total)
     else:
         entry.insert(tk.END, txt)
-    tkm.showinfo(txt, f"{txt}ボタンが押されました")
+    #tkm.showinfo(txt, f"{txt}ボタンが押されました")
 
 num_list = [n for n in range(10)]
 num_list.append("+")
@@ -23,32 +32,46 @@ num_list.append("-")
 num_list.append("*")
 num_list.append("/")
 num_list.append("=")
+num_list.append("C")
+num_list.append(".")
+num_list.append("AC")
+
 
 #print(num_list)
 
 for num in num_list:
-    button = tk.Button(root, text = str(num), font = ("",30), width = 4, height = 2)
-    button.bind("<1>", button_click)
     if type(num) is int:
-        if num % 3 == 0:
-            button.grid(row = 4 - math.ceil(num/3), column = 0)
+        button = tk.Button(root, text = str(num), font = ("",20), width = 4, height = 2, bg="#f5f5f5")
+        button.bind("<1>", button_click)
+        if num % 3 == 0 and num != 0:
+            button.grid(row = 6 - math.ceil(num/3), column = 0)
         elif num % 3 == 1:
-            button.grid(row = 4 - math.ceil(num/3), column = 2)
+            button.grid(row = 6 - math.ceil(num/3), column = 2)
         elif num % 3 == 2:
-            button.grid(row = 4 - math.ceil(num/3), column = 1)
+            button.grid(row = 6 - math.ceil(num/3), column = 1)
+        elif num == 0:
+            button.grid(row = 6 - math.ceil(num/3), column = 1)
     elif type(num) is str:
+        button = tk.Button(root, text = num, font = ("",20), width = 4, height = 2, bg="#c0c0c0")
+        button.bind("<1>", button_click)
         if num == "+":
-            button.grid(row = 1, column = 3)
+            button.grid(row = 5, column = 3)
         elif num == "-":
-            button.grid(row = 2, column = 3)
+            button.grid(row = 4, column = 3)
         elif num == "*":
             button.grid(row = 3, column = 3)
         elif num == "/":
-            button.grid(row = 4, column = 3)
+            button.grid(row = 2, column = 3)
         elif num == "=":
-            button.grid(row = 4, column = 2)
+            button.grid(row = 6, column = 3)
+        elif num == "C":
+            button.grid(row = 1, column = 2)
+        elif num == "AC":
+            button.grid(row = 1, column = 3)
+        elif num == ".":
+            button.grid(row = 6, column = 2)
         
-entry = tk.Entry(justify="right", width=14, font=("", 40))
-entry.grid(row = 0, columnspan=4)
+entry = tk.Entry(justify="right", width=16, font=("", 20))
+entry.grid(row = 0, columnspan=5)
 
 root.mainloop()
