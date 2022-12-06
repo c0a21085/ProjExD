0a47e5887c9345bd3c96bfac1d42bea4c7d061eb
tsujimura <c0a21085f4@edu.teu.ac.jp>
2022-12-06 16:53:43 +0900

    完成版

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
 import maze_maker as mm
 
 def key_down(event):
@@ -9,31 +10,43 @@ def key_down(event):
 def key_up(event):
     global key
     key = ""
-    #print(key)
+    #print(mx, my)
+
+#0~9までのキーを入力することで､それぞれの数値に対応したこうかとんを表示する。(未完成)
+def koukaton_change(event):
+    global key, tori
+    key = event.keysym
+    if key in fig_list:
+        tori = tk.PhotoImage(file=f"fig/{key}.png") 
+        canvas.create_image(cx, cy, image=tori, tag = "koukaton")
+        canvas.pack()        
 
 def main_proc():
     global cx, cy, mx, my
-    if key == "Up": 
-        my -= 1
-    if key == "Down": 
-        my += 1
-    if key == "Left": 
-        mx -= 1
-    if key == "Right": 
-        mx += 1
+    if key == "Up": my -= 1
+    if key == "Down": my += 1
+    if key == "Left": mx -= 1
+    if key == "Right": mx += 1
     if maze_list[mx][my] == 1: # 移動先が壁だったら
-        if key == "Up": 
-            my += 1
-        if key == "Down": 
-            my -= 1
-        if key == "Left": 
-            mx += 1
-        if key == "Right": 
-            mx -= 1    
+        if key == "Up": my += 1
+        if key == "Down": my -= 1
+        if key == "Left": mx += 1
+        if key == "Right": mx -= 1
     cx = 50 + 100 * mx
     cy = 50 + 100 * my
     canvas.coords("koukaton", cx, cy)
+    Goal()
     root.after(100 ,main_proc)
+
+#Goal地点に付いたらメッセージを表示する
+def Goal():
+    print(mx, my)
+    if mx == 13 and my == 7: #ゴール地点にいるなら
+        mes = tkm.showinfo("Congratulation","ゴールしました！！！")
+        if mes == "ok": #okボタンを押したら
+            root.destroy() #ゲームを終了する
+    else:
+        pass
 
 if __name__ == "__main__":
     key = ""
@@ -41,23 +54,23 @@ if __name__ == "__main__":
     cy = 150
     mx = 1
     my = 1
+    fig_list = [n for n in range(10)]
 
     root = tk.Tk()
     root.title("迷えるこうかとん")
-    
     tori = tk.PhotoImage(file="fig/2.png")
-
+    start = tk.PhotoImage(file="fig/start.png") #Start画像
+    goal = tk.PhotoImage(file="fig/goal.png") #Goal画像
     canvas = tk.Canvas(width = 1500, height = 900, bg = "black")
     maze_list = mm.make_maze(15, 9)
-    mm.print_maze(maze_list)
-    print(maze_list)
     mm.show_maze(canvas, maze_list)
+    canvas.create_image(150, 150, image=start) #スタート地点にStart画像を貼る
+    canvas.create_image(1350, 750, image=goal) #ゴール地点にGoal画像を貼る
     canvas.create_image(cx, cy, image=tori, tag = "koukaton")
     canvas.pack()
 
-    #mm.show_maze(canvas, mm.make_maze(15,9))
-
     root.bind("<KeyPress>", key_down)
+    #root.bind("<KeyPress>", koukaton_change, "+")
     root.bind("<KeyRelease>", key_up)
     main_proc()
 
