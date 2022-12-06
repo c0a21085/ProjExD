bb27b2fdc23bedb778b0a95648a015ccf6a7957b
tsujimura <c0a21085f4@edu.teu.ac.jp>
2022-12-06 15:27:58 +0900

    3限基本機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -12,22 +12,35 @@ def key_up(event):
     #print(key)
 
 def main_proc():
-    global cx, cy
-    if key == "Up":
-        cy -= 20
-    elif key == "Down":
-        cy += 20
-    elif key == "Left":
-        cx -= 20
-    elif key == "Right":
-        cx += 20
+    global cx, cy, mx, my
+    if key == "Up": 
+        my -= 1
+    if key == "Down": 
+        my += 1
+    if key == "Left": 
+        mx -= 1
+    if key == "Right": 
+        mx += 1
+    if maze_list[mx][my] == 1: # 移動先が壁だったら
+        if key == "Up": 
+            my += 1
+        if key == "Down": 
+            my -= 1
+        if key == "Left": 
+            mx += 1
+        if key == "Right": 
+            mx -= 1    
+    cx = 50 + 100 * mx
+    cy = 50 + 100 * my
     canvas.coords("koukaton", cx, cy)
-    root.after(50 ,main_proc)
+    root.after(100 ,main_proc)
 
 if __name__ == "__main__":
     key = ""
-    cx = 300
-    cy = 400
+    cx = 150
+    cy = 150
+    mx = 1
+    my = 1
 
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -35,7 +48,10 @@ if __name__ == "__main__":
     tori = tk.PhotoImage(file="fig/2.png")
 
     canvas = tk.Canvas(width = 1500, height = 900, bg = "black")
-    mm.show_maze(canvas,mm.make_maze(15,9))
+    maze_list = mm.make_maze(15, 9)
+    mm.print_maze(maze_list)
+    print(maze_list)
+    mm.show_maze(canvas, maze_list)
     canvas.create_image(cx, cy, image=tori, tag = "koukaton")
     canvas.pack()
 
