732910d62143aac754c2f09f65f7e38213d879d4
tsujimura <c0a21085f4@edu.teu.ac.jp>
2022-12-06 14:18:35 +0900

    問題３

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,7 +4,10 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
     
+    image = tk.PhotoImage(file="fig/2.png")
+
     canvas = tk.Canvas(width = 1500, height = 900, bg = "black")
+    canvas.create_image(300, 400, image=image)
     canvas.pack()
 
     root.mainloop()
