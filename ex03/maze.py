c6d8e782fe9f711afc434b70a0a610f3e509f930
tsujimura <c0a21085f4@edu.teu.ac.jp>
2022-12-06 14:44:35 +0900

    問題７

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,24 +3,42 @@ import tkinter as tk
 def key_down(event):
     global key
     key = event.keysym
+    #print(key)
 
 def key_up(event):
     global key
     key = ""
+    #print(key)
+
+def main_proc():
+    global cx, cy
+    if key == "Up":
+        cy -= 20
+    elif key == "Down":
+        cy += 20
+    elif key == "Left":
+        cx -= 20
+    elif key == "Right":
+        cx += 20
+    canvas.coords("koukaton", cx, cy)
+    root.after(100 ,main_proc)
 
 if __name__ == "__main__":
     key = ""
+    cx = 300
+    cy = 400
 
     root = tk.Tk()
     root.title("迷えるこうかとん")
     
-    image = tk.PhotoImage(file="fig/2.png")
+    tori = tk.PhotoImage(file="fig/2.png")
 
     canvas = tk.Canvas(width = 1500, height = 900, bg = "black")
-    canvas.create_image(300, 400, image=image)
+    canvas.create_image(cx, cy, image=tori, tag = "koukaton")
     canvas.pack()
 
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
+    main_proc()
 
     root.mainloop()
