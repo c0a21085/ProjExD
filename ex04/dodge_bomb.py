fb331430410d08f452161f6f9dca8d9a0ae46cce
tsujimura <c0a21085f4@edu.teu.ac.jp>
2022-12-13 14:15:49 +0900

    問題２

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -5,17 +5,20 @@ def main():
     clock = pg.time.Clock() #時間計測用オブジェクト
 
     pg.display.set_caption("逃げろこうかとん！") #タイトルバーに「」を表示
-    scrn_sfc = pg.display.set_mode((800, 450)) #1600x900の画面Surfaceを生成
+    scrn_sfc = pg.display.set_mode((1600, 900)) #1600x900の画面Surfaceを生成
 
     bg_sfc = pg.image.load("fig/pg_bg.jpg")
-    bg_sfc = pg.transform.rotozoom(bg_sfc, 0, 2.0)
+    #bg_sfc = pg.transform.scale(bg_sfc, (1200, 600))
     bg_rct = bg_sfc.get_rect() #Rect
-    bg_rct.center = 400, 300
-    scrn_sfc.blit(bg_sfc, bg_rct) #blid 
+    #bg_rct.center = 400, 300
 
-    pg.display.update()
-    clock.tick(0.2) #1fpsの時を刻む
-    
+    while True:
+        scrn_sfc.blit(bg_sfc, bg_rct) #blid  
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+        pg.display.update()
+        clock.tick(1000) #1fpsの時を刻む
     
 if __name__ == "__main__":
     pg.init()
