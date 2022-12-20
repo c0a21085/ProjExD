ee603e11128b517a455c929ab91f9fe79b33865c
tsujimura <c0a21085f4@edu.teu.ac.jp>
2022-12-20 15:07:01 +0900

    授業(base)

--- ex05/dodge_bomb.py
+++ ex05/dodge_bomb.py
@@ -2,66 +2,67 @@ import pygame as pg
 import random
 import sys
 
+
 class Screen:
-    def __init__(self, title = "逃げろ！こうかとん", size = (1600, 900), image = "fig/pg_bg.jpg"):
-        pg.display.set_caption(title)
-        self.sfc = pg.display.set_mode(size)
+    def __init__(self, title, wh, img_path):
+        pg.display.set_caption(title) 
+        self.sfc = pg.display.set_mode(wh)
         self.rct = self.sfc.get_rect()
-        self.bgi_sfc = pg.image.load(image)
-        self.bgi_rct = self.bgi_sfc.get_rect()  
+        self.bgi_sfc = pg.image.load(img_path)
+        self.bgi_rct = self.bgi_sfc.get_rect() 
 
-    def blid(self):
+    def blit(self):
         self.sfc.blit(self.bgi_sfc, self.bgi_rct) 
-        
 
-class Bird():
+
+class Bird:
     key_delta = {
         pg.K_UP:    [0, -1],
         pg.K_DOWN:  [0, +1],
         pg.K_LEFT:  [-1, 0],
         pg.K_RIGHT: [+1, 0],
-    }   
+    }
 
-    def __init__(self, image = "fig/6.png", ratio = 2.0, xy = (900, 400)):
-        self.sfc = pg.image.load(image)
+    def __init__(self, img_path, ratio, xy):
+        self.sfc = pg.image.load(img_path)
         self.sfc = pg.transform.rotozoom(self.sfc, 0, ratio)
         self.rct = self.sfc.get_rect()
-        self.rct.center = xy 
+        self.rct.center = xy
 
-    def blid(self, scr:Screen):
-        scr.sfc.blid(self.sfc, self.rct)
+    def blit(self, scr:Screen):
+        scr.sfc.blit(self.sfc, self.rct)
 
     def update(self, scr:Screen):
         key_dct = pg.key.get_pressed()
         for key, delta in Bird.key_delta.items():
             if key_dct[key]:
                 self.rct.centerx += delta[0]
-                self.rct.centery += delta[1]
-            if check_bound(self.rct, self.rct) != (+1, +1):
+                self.rct.centery += delta[1]  
+            if check_bound(self.rct, scr.rct) != (+1, +1):
                 self.rct.centerx -= delta[0]
                 self.rct.centery -= delta[1]
-        self.blid(scr)
+        self.blit(scr)                    
 
 
-class Bomb():
-    def __init__(self, color = (255, 0, 0), rad = 10, vxy = (+1, +1)):
-        self.sfc = pg.Surface((2*rad, 2*rad))
+class Bomb:
+    def __init__(self, color, rad, vxy, scr:Screen):
+        self.sfc = pg.Surface((2*rad, 2*rad)) 
         self.sfc.set_colorkey((0, 0, 0))
         pg.draw.circle(self.sfc, color, (rad, rad), rad)
         self.rct = self.sfc.get_rect()
-        self.rct.centerx = random.randint(0, self.rct.width)
-        self.rct.centery = random.randint(0, self.rct.height)
+        self.rct.centerx = random.randint(0, scr.rct.width)
+        self.rct.centery = random.randint(0, scr.rct.height)
         self.vx, self.vy = vxy
 
     def blit(self, scr:Screen):
-        scr.sfc.blid(self.sfc, self.rct)
+        scr.sfc.blit(self.sfc, self.rct)
 
     def update(self, scr:Screen):
-        self.rct.move_ip(self.vx, self.vy) 
-        yoko, tate = check_bound(self.rct, self.rct)
+        self.rct.move_ip(self.vx, self.vy)
+        yoko, tate = check_bound(self.rct, scr.rct)
         self.vx *= yoko
         self.vy *= tate
-        self.blid(scr)
+        self.blit(scr)
 
 
 def check_bound(obj_rct, scr_rct):
@@ -80,30 +81,30 @@ def check_bound(obj_rct, scr_rct):
 
 def main():
     clock =pg.time.Clock()
-    
-    scr = Screen()
 
-    bird = Bird()
-    bird.update(scr)
+    scr = Screen("逃げろ！こうかとん", (1600,900), "fig/pg_bg.jpg")
 
-    bomb = Bomb()
-    bomb.update(scr)
+    kkt = Bird("fig/6.png", 2.0, (900,400))
+    kkt.update(scr)
 
-    # 練習２
-    while True:
-        scr.blid()
-  
+    bkd = Bomb((255, 0, 0), 10, (+1, +1), scr)
+    bkd.update(scr)
+
+    while True:        
+        scr.blit()
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
 
-        bird.update(scr)
-        bomb.update(scr)       
-        if bird.rct.colliderect(bomb.rct):
+        kkt.update(scr)
+        bkd.update(scr)
+        if kkt.rct.colliderect(bkd.rct):
             return
 
         pg.display.update()
         clock.tick(1000)
+
 
 if __name__ == "__main__":
     pg.init()
